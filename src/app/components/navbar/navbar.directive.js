30b95538551eb7a72bac1f2f62f8899f3d529a59
juandjara <juanorigami@gmail.com>
2016-05-20 01:13:48 +0200

    typeahead

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -21,9 +21,40 @@
     return directive;
 
     /** @ngInject */
-    function NavbarController(moment) {
+    function NavbarController(moment, $http) {
       var vm = this;
-
+      vm.searchRes;
+      vm.search = "";
+      vm.searchOmdb = searchOmdb;
+      vm.parseOmdb = parseOmdb;
+      
+      function searchOmdb(query){
+        if(!query || query.length < 2){
+          return;
+        }
+        var url = "http://api.tvmaze.com/search/shows?q="+query;
+        return $http.get(url).then(function(res){
+          vm.searchRes = res.data;
+          return res.data;
+        })
+      }
+      
+      function parseOmdb(obj){
+        console.log("NavbarController: parsing "+angular.toJson(obj));
+        if(typeof obj !== 'object'){
+          return "void";
+        }
+        return obj.show.externals.imdb+": "+obj.show.name;
+      }
+      
+      function normalizeArray(obj){
+        if(Array.isArray(obj)){
+          return obj;
+        }else{
+          return [obj];
+        }
+      }
+      
     }
   }
 
