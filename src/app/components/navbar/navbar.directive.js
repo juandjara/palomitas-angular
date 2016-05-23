e476dd21691b42d3de95b80e27bdfb09f02b6d32
juandjara <juanorigami@gmail.com>
2016-05-23 22:06:22 +0200

    cambio de dominio

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -32,7 +32,7 @@
         if(!query || query.length < 2){
           return;
         }
-        var url = "https://tvmaze.fuken.xyz"
+        var url = "https://tvapi.fuken.xyz"
         url += "/search/shows?q="+query;
         return $http.get(url).then(function(res){
           vm.searchRes = res.data;
